02fdbdbcd4c0ff8a28db8602e1b6e4d6acc79320
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-02-20 13:53:01 +0900

    add tab menu

--- js_basic_event/main.js
+++ js_basic_event/main.js
@@ -5,7 +5,7 @@ import { countUp } from "./utils/countUp.js";
 //   setResultContainer,
 // } from "./module/selectCard.js";
 // import { setMbtiSection } from "./module/mbtiSelect.js";
-// import { setTabMenu } from "./module/tabMenu.js";
+import { setTabMenu } from "./module/tabMenu.js";
 // import { setShareURLButton } from "./module/share.js";
 
 const data = {
@@ -18,7 +18,7 @@ const participateDOM = document.getElementById(
 );
 countUp(participateDOM, data.participate, 5);
 
-// setTabMenu();
+setTabMenu();
 
 // setSelectCards();
 // setSelectButton();
